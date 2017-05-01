4c58cb25cfc6db79ecfeb5e2b1e5c5c32f9259bc
fred.vogt <fred.vogt@dominodatalab.com>
2017-04-30 23:18:55 -0700

    Turn off 'Can one of the admins ...' messages in Github PRs

--- src/main/java/org/jenkinsci/plugins/ghprb/GhprbPullRequest.java
+++ src/main/java/org/jenkinsci/plugins/ghprb/GhprbPullRequest.java
@@ -123,14 +123,7 @@ public class GhprbPullRequest {
         GHUser author = pr.getUser();
         String reponame = repo.getName();
 
-        if (ghprb.isWhitelisted(author)) {
-            setAccepted(true);
-        } else {
-            logger.log(Level.INFO,
-                       "Author of #{0} {1} on {2} not in whitelist!",
-                       new Object[] { id, author.getLogin(), reponame });
-            repo.addComment(id, GhprbTrigger.getDscp().getRequestForTestingPhrase());
-        }
+        setAccepted(true);
 
         logger.log(Level.INFO,
             "Created Pull Request #{0} on {1} by {2} ({3}) updated at: {4} SHA: {5}",
